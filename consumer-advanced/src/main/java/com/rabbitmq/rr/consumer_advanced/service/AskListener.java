b7bab5188bebb4806cbc342cd4afbc1cc703516d
Rodrigo Feitosa <rodrigofeitosarodrigues@gmail.com>
2025-01-07 21:34:00 -0300

    Adicionando confirmação manual de leitura de uma mensagem pelo consumer

---
+++ consumer-advanced/src/main/java/com/rabbitmq/rr/consumer_advanced/service/AskListener.java
@@ -0,0 +1,31 @@
+package com.rabbitmq.rr.consumer_advanced.service;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.rr.consumer_advanced.domain.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class AskListener {
+
+    private final Logger logger = LoggerFactory.getLogger(AskListener.class);
+
+    @RabbitListener(queues = "FIRST-QUEUE-ADVANCED", ackMode = "MANUAL")
+    public void askListener(Message<Person> message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long tag) throws IOException {
+        logger.info("Received Message: {}", message);
+        if (message.getPayload().collageCompletedYear() == null) {
+            logger.warn("Message wrong");
+            channel.basicNack(tag != null ? tag : 0L, false, false);
+        } else {
+            logger.info("message Ok: {}", message.getHeaders());
+            channel.basicAck(tag != null ? tag : 0L, false);
+        }
+    }
+}
